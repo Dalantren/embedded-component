d7368b7c8b566137f1acc72f0d433e09dafec3a1
Ivanitskii Artem <dalantren@gmail.com>
2025-03-02 15:51:24 +0100

    Added examples of working and not working solutions

--- child/src/web-component.ts
+++ child/src/web-component.ts
@@ -1,5 +1,19 @@
+import { createApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { createCustomElement } from '@angular/elements';
+
+// For iport as a module:
 
 export const emConfig = appConfig;
 export const emComponent = AppComponent;
+
+//------------------------------------------
+
+// For import as a text/javascript:
+
+// createApplication(appConfig)
+//   .then(appRef => {
+//     const element = createCustomElement(AppComponent, { injector: appRef.injector });
+//     customElements.define('child-app', element);
+//   });
--- parent/src/app/app.component.ts
+++ parent/src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { ApplicationConfig, ApplicationRef, Component, createComponent, CUSTOM_ELEMENTS_SCHEMA, EnvironmentInjector, inject, Injector, NgZone, PlatformRef, Renderer2, signal, Type, viewChild, ViewContainerRef } from '@angular/core';
+import { ApplicationConfig, ApplicationRef, Component, createComponent, CUSTOM_ELEMENTS_SCHEMA, EnvironmentInjector, importProvidersFrom, inject, Injector, makeEnvironmentProviders, NgZone, PlatformRef, Renderer2, signal, Type, viewChild, ViewContainerRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { createCustomElement } from '@angular/elements';
 import { TestComponent } from './test.component';
 import { createApplication } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [TestComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  templateUrl: './app.component.html',
+    selector: 'app-root',
+    imports: [TestComponent],
+    standalone: true,
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
   private container = viewChild('customSelectorContainer', { read: ViewContainerRef });
@@ -21,24 +21,33 @@ export class AppComponent {
   private zone = inject(NgZone);
   private abc = inject(PlatformRef);
   private appRef = inject(ApplicationRef);
-  private component!: Type<any>;
-  private config!: ApplicationConfig;
 
   ngOnInit() {
-    this.loadScript().then(() => {
-      const childElement: HTMLElement = this.renderer.createElement('child-app');
-      // this.container()?.element.nativeElement.appendChild(childElement);
+    console.log('this.injector', this.injector);
+    console.log('this.envInjector', this.envInjector);
+    console.log('this.appRefInjector', this.appRef.injector);
+    console.log('this.platformInjector', this.abc.injector);
 
-      const comp = createComponent(this.component, {
-        environmentInjector: this.appRef.injector,
-        hostElement: this.container()?.element.nativeElement,
-        elementInjector: this.injector,
-      });
-      console.log(comp);
+    // Not working examples for loadScript1() and loadScript2():
+    this.loadScript2().then(({ emConfig, emComponent }) => {
+      const customElement = createCustomElement(emComponent, { injector: this.envInjector });
+      customElements.define('child-app', customElement);
+
+      const childElement: HTMLElement = this.renderer.createElement('child-app');
+      debugger;
+      this.container()?.element.nativeElement.appendChild(childElement);
     });
+
+    // Working example with import like a regular script:
+
+    // this.loadScript3().then(() => {
+    //   const childElement: HTMLElement = this.renderer.createElement('child-app');
+    //   debugger;
+    //   this.container()?.element.nativeElement.appendChild(childElement);
+    // });
   }
 
-  private loadScript(): Promise<void> {
+  private loadScript1(): Promise<{ emConfig: ApplicationConfig, emComponent: Type<any> }> {
     const src = 'http://127.0.0.1:5500/dist/child/main.js';
     return new Promise((resolve, reject) => {
       const scriptElement = document.createElement('script');
@@ -46,22 +55,55 @@ export class AppComponent {
       scriptElement.id = `test-script`;
       scriptElement.type = 'module';
 
-      this.abc.injector
       scriptElement.onload = () => {
         import(/* @vite-ignore */ src)
           .then(({ emConfig, emComponent }) => {
-            this.component = emComponent;
-            this.config = emConfig;
-            return this.zone.runOutsideAngular(() => createApplication(emConfig));
-          })
-          // .then(({ emConfig, emComponent }) => {
-          .then(appRef => {
-            const customElement = createCustomElement(this.component, { injector: appRef.injector });
-            customElements.define('child-app', customElement);
-            resolve();
+            resolve({ emConfig, emComponent });
           })
           .catch(err => console.error(err));
       }
+      scriptElement.onerror = () => reject(new Error('failed to load custom app'));
+      document.body.appendChild(scriptElement);
+    });
+  }
+
+  private loadScript2(): Promise<{ emConfig: ApplicationConfig, emComponent: Type<any> }> {
+    const src = 'http://127.0.0.1:5500/dist/child/main.js';
+    return new Promise((resolve, reject) => {
+      const scriptElement = document.createElement('script');
+      scriptElement.src = src;
+      scriptElement.id = `test-script`;
+      scriptElement.type = 'module';
+
+      let emConfig: ApplicationConfig;
+      let emComponent: Type<any>;
+
+      scriptElement.onload = () => {
+        import(/* @vite-ignore */ src)
+          .then(({ emConfig: config, emComponent: component }) => {
+            emComponent = component;
+            emConfig = config ;
+            return this.zone.runOutsideAngular(() => createApplication(config));
+          })
+          .then(appRef => {
+            resolve({ emConfig, emComponent });
+          })
+          .catch(err => console.error(err));
+      }
+      scriptElement.onerror = () => reject(new Error('failed to load custom app'));
+      document.body.appendChild(scriptElement);
+    });
+  }
+
+  private loadScript3(): Promise<void> {
+    const src = 'http://127.0.0.1:5500/dist/child/main.js';
+    return new Promise((resolve, reject) => {
+      const scriptElement = document.createElement('script');
+      scriptElement.src = src;
+      scriptElement.id = `test-script`;
+      scriptElement.type = 'text/javascript';
+
+      scriptElement.onload = () => resolve();
       scriptElement.onerror = () => reject(new Error('failed to load custom app'));
       document.body.appendChild(scriptElement);
     });
